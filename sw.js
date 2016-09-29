841054359af53cb0fc4c1d0fb73b50d1650b9b69
tombye <tombaromba@gmail.com>
2016-09-29 21:53:51 +0100

    Add install behaviours

--- sw.js
+++ sw.js
@@ -1,1 +1,16 @@
 // serviceworker script
+self.addEventListener('install', event => {
+	function onInstall () {
+		return caches.open('static')
+			.then(cache => cache.addAll([
+					'/serviceworker_test/images/panda.png'
+				])
+			);
+	}
+
+	event.waitUntil(onInstall(event));
+});
+
+self.addEventListener('activate', event => {
+
+});
